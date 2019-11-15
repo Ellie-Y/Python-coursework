dba1efc2b787af5ebd37891db6a75c326e46bd3b
Ellie-Y <ellie.ye22@gamil.com>
2019-11-15 08:03:25 +0000

    update

--- Processing-system.py
+++ Processing-system.py
@@ -55,14 +55,6 @@ def retrieve_tasks():
     tasks_data = connect.execute('SELECT ID, ARRIVAL, DURATION FROM tasks ORDER BY ARRIVAL ASC')
     return tasks_data
 
-#类是传入参数然后操作参数实现想实现的功能
-#外界使用类创建对象，然后让对象调用方法
-
-'''目前的问题：
-1. 当队列满了之后任务如何等待
-2. 最后的总时间不对
-3. 正则
-'''
 
 class SimulateSystem():
     def __init__(self, task_list, clock, queue):
@@ -73,14 +65,12 @@ class SimulateSystem():
 
     def match_id(self):
         approved_tasks = []
-        # findall() ? order of a-zA-Z
-        # ?是否有，没有就走到前面再一次匹配
         matched_id = '[a-zA-Z]+[^a-zA-Z0-9]+'
         for row in self.task_list:
-            now_time = self.clock + row[1]  # 让时钟更新成arrival时间
+            now_time = self.clock + row[1]  # update clock
             print('** [%s] : Task [%s --- %f] with duration [%d] enters the system'
                   % (now_time, row[0], row[1], row[2]))
-            #匹配 ID
+    
             if not re.match(matched_id, row[0]):
                 print('** Task [%s] unfeasible and discarded' % row[0])
             else:
@@ -88,12 +78,11 @@ class SimulateSystem():
                 print('** Task [%s] accepted.' % row[0])
         self.task_list = approved_tasks
         self.clock = now_time
-        print('通过的任务！！')
+        print('passed tasks！！')
         print(self.task_list)
 
 
     def save_data(self):
-        #把任务遍历存到队列里面
         for task in self.task_list:
             if not self.queue.full():
                 self.queue.put_nowait(task)
@@ -114,15 +103,13 @@ class SimulateSystem():
             self.clock += task[2]
             print('** [%f] : Task [%s] completed.' % (self.clock, task[0]))
             if self.queue.empty():
-                print('队列里面的任务为空')
+                print('queue is empty')
                 final_time = self.clock
                 break
         print('** [%f] : SIMULATION COMPLETED. **' % final_time)
 
 
     def generate_processors(self):
-        #主进程和子进程的关系所以要用 Manager()
-        #第一个进程把筛选过的任务存入queue
         process1 = multiprocessing.Process(target=self.save_data, args=())
         process1.start()
         process1.join()
@@ -140,7 +127,7 @@ class SimulateSystem():
 if __name__ == '__main__':
     connect = sqlite3.connect('dataset-class.db')
     connect_database()
-    #把从数据库获取的内容传入
+    #pass the content from database
     task_list = retrieve_tasks()
     clock = generate_clock()
     queue = multiprocessing.Manager().Queue()
@@ -150,5 +137,4 @@ if __name__ == '__main__':
     simulator = SimulateSystem(task_list, clock, queue)
     simulator.match_id()
     simulator.generate_processors()
-    #没有调用process_data所以时间没有更新, 调用了也没有更新？
     print('** [%f] : SIMULATION COMPLETED. **' % simulator.clock)
